87e8ed011d3bc991ac4fc8f6abb5aedefb46f6af
wesbin <leegosoo11@naver.com>
2021-11-08 15:32:31 +0900

    spy - 한수

---
+++ spy/함수/한수.java
@@ -0,0 +1,37 @@
+package 함수;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 한수 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int a = Integer.parseInt(str);
+//        int a = 1;
+
+        if (a < 100) {
+            System.out.println(a);
+            return;
+        }
+        int cnt = 0;
+        if (a == 1000) {
+            cnt = -1;
+        }
+        for (int i = 100; i <= a; i++) {
+            int temp = i;
+            int first = temp % 10;
+            temp /= 10;
+            int second = temp % 10;
+            int diff = first - second;
+            temp /= 10;
+            if ((second - diff) == (temp % 10)) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt + 99);
+    }
+}
