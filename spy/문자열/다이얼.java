eb72d2b5c2741dd696658d43c37d268747b8a820
wesbin <leegosoo11@naver.com>
2021-11-18 20:10:45 +0900

    다이얼

--- spy/문자열/다이얼.java
+++ spy/문자열/다이얼.java
@@ -7,11 +7,10 @@ public class 다이얼 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int answer = 0;
+        int[] secondArr = {3,3,3,4,4,4,5,5,5,6,6,6,7,7,7,8,8,8,8,9,9,9,10,10,10,10};
         char[] SArr = sc.nextLine().toCharArray();
         for (char c : SArr) {
-            System.out.println(c - 64);
-            System.out.println(c + "|||" + (((c - 64) / 3) + 2));
-            answer += ((c - 64) / 3) + 2;
+            answer += secondArr[c - 65];
         }
         System.out.println(answer);
     }
