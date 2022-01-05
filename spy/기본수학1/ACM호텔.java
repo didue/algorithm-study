9bfbfda28674eccf0c7305066d629c99c43f5649
wesbin <leegosoo11@naver.com>
2022-01-05 18:01:01 +0900

    왜 안되지?

---
+++ spy/기본수학1/ACM호텔.java
@@ -0,0 +1,35 @@
+package 기본수학1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ACM호텔 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int repeat = Integer.parseInt(br.readLine());
+//        int repeat = 1;
+        for (int i = 0; i < repeat; i++) {
+
+            String[] s = br.readLine().split(" ");
+//            String[] s = {"6", "12", "10"};
+            int height = Integer.parseInt(s[0]);
+            int width = Integer.parseInt(s[1]);
+            int nth = Integer.parseInt(s[2]);
+
+            int floor = nth % height;
+            int nthRoom = nth / height + 1;
+
+            if (floor == 0) {
+                floor = height;
+                nthRoom--;
+            }
+
+            System.out.println(String.format("%0" + (int) (Math.log10(height) + 1) + "d", floor) + String.format("%02d", nthRoom));
+
+        }
+    }
+}
--- spy/기본수학1/달팽이는올라가고싶다.java
+++ spy/기본수학1/달팽이는올라가고싶다.java
@@ -1,9 +1,27 @@
 package 기본수학1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class 달팽이는올라가고싶다 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        int day = Integer.parseInt(s[0]);
+//        int day = 3;
+        int night = Integer.parseInt(s[1]);
+//        int night = 2;
+        int top = Integer.parseInt(s[2]);
+//        int top = 10;
+        int dayClimbed = day - night;
+        int beforeLastDay = top - day;
+        int answer = beforeLastDay / dayClimbed + 1;
+        if (beforeLastDay % dayClimbed > 0) {
+            answer++;
+        }
+        System.out.println(answer);
     }
 }
