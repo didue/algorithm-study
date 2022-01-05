6b00b91705b96f30f582e9fae4b1ab8a9c1bdfe7
wesbin <leegosoo11@naver.com>
2022-01-05 11:55:07 +0900

    분수찾기 컷!

---
+++ spy/기본수학1/달팽이는올라가고싶다.java
@@ -0,0 +1,9 @@
+package 기본수학1;
+
+public class 달팽이는올라가고싶다 {
+
+    public static void main(String[] args) {
+
+
+    }
+}
--- spy/기본수학1/분수찾기.java
+++ spy/기본수학1/분수찾기.java
@@ -1,30 +1,38 @@
 package 기본수학1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class 분수찾기 {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        int nth = 1;
-        int sum = 0;
-        int maxNum = 0;
-        for (int i = 1; i < 10; i++) {
-            sum += i;
-            if (nth < sum) {
-                maxNum = i;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int target = Integer.parseInt(br.readLine());
+//        int target = 14;
+        int max = 0;
+        int i = 1;
+        boolean oddNthTrigger = true;
+        while (true) {
+            max += i;
+            oddNthTrigger = !oddNthTrigger;
+            if (target <= max) {
                 break;
             }
+            i++;
         }
-        int leftnth = (nth - (sum - maxNum)) - 1;
-        if (maxNum % 2 == 0) {
-            // 짝수 1 / n
-            int left = 1 + leftnth;
-            int right = maxNum - leftnth;
-            System.out.println(left + "/" + right);
+        int gap = max - target;
+        int numerator;
+        int denominator;
+        if (oddNthTrigger) {
+            numerator = i - gap;
+            denominator = 1 + gap;
         } else {
-            // 홀수 n / 1
-            int left = maxNum - leftnth;
-            int right = 1 + leftnth;
-            System.out.println(left + "/" + right);
+            numerator = 1 + gap;
+            denominator = i - gap;
         }
+
+        System.out.println(numerator + "/" + denominator);
     }
 }
